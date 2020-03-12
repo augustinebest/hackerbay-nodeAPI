5c8df507aa56cce2b0a246589ef8d0c74fe14971
Igwe Augustine <augustusbest00@gmail.com>
2020-03-12 04:49:41 -0700

    added json-patch routes

--- api/routes/authorization.js
+++ api/routes/authorization.js
@@ -1,7 +1,9 @@
 import express from 'express';
 import authenticationController from '../controllers/authorization';
+import jsonPatchController from '../controllers/jsonpatch';
 const router = express.Router();
 
 router.post('/login', authenticationController.login);
+router.patch('/jsonpatcher', jsonPatchController.patchPayload);
 
 export default router;
