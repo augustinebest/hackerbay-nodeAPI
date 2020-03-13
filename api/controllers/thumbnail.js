39ba35ecde7b07cc31b8545010acb49b216d972a
Igwe Augustine <augustusbest00@gmail.com>
2020-03-13 01:57:48 -0700

    finally fixed the generate thumbnail controller

--- api/controllers/thumbnail.js
+++ api/controllers/thumbnail.js
@@ -1,32 +1,27 @@
-import path from 'path';
-import fs from 'fs';
-import axios from 'axios';
 import sharp from 'sharp';
+import path from 'path';
+import axios from 'axios';
+import fs from 'fs';
+const dir = path.join(__dirname, '/../../thumbnails/');
 
 class Thumbnail {
     generateThumbnail(req, res) {
-        const imageUri = req.query.imageUrl;
-        console.log(imageUri)
+        const filename = req.query.imageUrl;
         axios({
             method: "get",
-            url: imageUri,
+            url: filename,
             responseType: "stream"
         }).then(function (response) {
-            const newName = Date.now();
-            response.data.pipe(fs.createWriteStream(`./thumbnails/${newName}.jpg`));
-            const filePath = path.join(__dirname, `../../thumbnails${newName}.jpg`)
-            // const downloadImagePath = filePath;
-            sharp(filePath)
-                .resize({
-                    width: 50,
-                    height: 50
-                })
-                // .toBuffer()
-                .toFile(`./thumbnails/resize/${filePath}.jpg`, (err) => {
-                    if (err) throw err;
-                    res.json(`./thumbnails/resize/${filePath}.jpg`)
-                })
-                // .then();
+            const newName = Date.now(), w = 50, h = 50;
+            const downloadImagePath = path.resolve(dir, `${newName}.jpg`)
+            const writeImage = fs.createWriteStream(downloadImagePath);
+            response.data.pipe(writeImage);
+            const thumbnailPath = path.resolve(dir, `resize/thumb-${newName}-${w}x${h}.jpg`);
+            writeImage.on('close', () => {
+                sharp(downloadImagePath).resize({ width: w, height: h }).toFile(thumbnailPath).then(re => {
+                    res.send(path.join(__dirname, `/../../thumbnails/resize/${newName}-${w}x${h}.jpg`))
+                }).catch(err => console.log('error just happened', err))
+            })
         });
     }
 }
